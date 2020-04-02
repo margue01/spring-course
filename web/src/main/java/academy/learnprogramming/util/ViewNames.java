8809696067786b161e9b70dc17ef643e12124ecf
Martin Guest <guest.martin@gmail.com>
2020-04-02 17:32:32 +0100

    Add more views, service and thymeleaf preprocessing

--- web/src/main/java/academy/learnprogramming/util/ViewNames.java
+++ web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -3,5 +3,7 @@ package academy.learnprogramming.util;
 public final class ViewNames {
 
     public static final String HOME = "home";
+    public static final String PLAY = "play";
+    public static final String GAME_OVER = "game-over";
     private ViewNames() {}
 }
